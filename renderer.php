b650cb7c10f36183931a91e5001b89e9177ac1d5
Adrian Czermak <adrian.czermak@univie.ac.at>
2019-12-16 17:21:50 +0100

    new mustache template

--- renderer.php
+++ renderer.php
@@ -23,9 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-
 defined('MOODLE_INTERNAL') || die();
-
 
 /**
  * Base class for generating the bits of output common to multiple choice
@@ -36,52 +34,43 @@ defined('MOODLE_INTERNAL') || die();
  */
 class qtype_flashcard_renderer extends qtype_with_combined_feedback_renderer {
 
-  public function formulation_and_controls(question_attempt $qa,
-                                           question_display_options $options) {
-    global $PAGE;
-    $question = $qa->get_question();
-    foreach ($question->answers as $answer) {
-      $ans = $answer;
+    public function formulation_and_controls(question_attempt $qa,
+            question_display_options $options) {
+        global $PAGE;
+        $question = $qa->get_question();
+        foreach ($question->answers as $answer) {
+            $ans = $answer;
+        }
+
+        $context = ['qaid' => $qa->get_database_id()];
+        $PAGE->requires->js_call_amd('qtype_flashcard/flipquestion', 'init', $context);
+        $renderer = $PAGE->get_renderer('core');
+        $templatecontext['questiontext'] = $question->format_questiontext($qa);
+        $templatecontext['answer'] = $question->format_text(
+                $ans->answer, $ans->answerformat,
+                $qa, 'question', 'answer', $ans->id);
+        $templatecontext['flipid'] = 'qflashcard-flipbutton-' . $qa->get_database_id();
+        $templatecontext['fliptext'] = get_string('flipbutton', 'qtype_flashcard');
+        $templatecontext['iwasrightid'] = 'qflashcard-iwasrightbutton-' . $qa->get_database_id();
+        $templatecontext['iwaswrongid'] = 'qflashcard-iwaswrongbutton-' . $qa->get_database_id();
+        $templatecontext['iwasrighttext'] = $this->pix_icon('t/check', '') . get_string('iwasrightbutton', 'qtype_flashcard');
+        $templatecontext['iwaswrongtext'] = $this->pix_icon('e/cancel', '') . get_string('iwaswrongbutton', 'qtype_flashcard');
+
+        $content = $renderer->render_from_template('qtype_flashcard/questionview', $templatecontext);
+      
+        $result = html_writer::tag('div', $content,
+                array('id' => 'qflashcard-flipcontainer-' . $qa->get_database_id(), 'class' => 'qflashcard-flipcontainer'));
+        if ($qa->get_state() == question_state::$invalid) {
+            $result .= html_writer::nonempty_tag('div',
+                    $question->get_validation_error($qa->get_last_qt_data()),
+                    array('class' => 'validationerror'));
+        }
+
+        return $result;
     }
-    $flipper = '';
-    $flipcontainercontent = html_writer::tag('div', $question->format_questiontext($qa),
-      array('class' => 'qtext qflashcard-question align-self-stretch'));
 
-    $flipcontainercontent .= html_writer::start_tag('div', array('class' => 'ablock qflashcard-ablock align-self-stretch'));
-
-    $flipcontainercontent .= html_writer::start_tag('div', array('class' => 'answer'));
-    $flipcontainercontent .= html_writer::tag('div', $question->format_text(
-        $ans->answer, $ans->answerformat,
-        $qa, 'question', 'answer', $ans->id),
-        array('class' => 'qanswer qflashcard-answer qflashcard-flipin')) . "\n";
-    $flipcontainercontent .= html_writer::end_tag('div'); // Answer.
-
-    $flipcontainercontent .= html_writer::end_tag('div'); // Ablock.
-    $result = '';
-    $flipper .= html_writer::div($flipcontainercontent, 'qflashcard-flipper d-flex flex-column');
-    $flipper .= html_writer::start_tag('div', array('class' => 'row no-gutters mt-2'));
-    $flipper .= html_writer::tag('button', get_string('flipbutton', 'qtype_flashcard'), ['class' => 'qflashcard-flipbutton btn btn-primary', 'id' => 'qflashcard-flipbutton-' . $qa->get_database_id()]);
-    $flipper .= html_writer::end_tag('div');
-
-    $flipper .= html_writer::start_tag('div', array('class' => 'row d-inline-flex no-gutters mt-3'));
-    $flipper .= html_writer::tag('button', $this->pix_icon('t/check', '') . get_string('iwasrightbutton', 'qtype_flashcard'), ['class' => 'qflashcard-iwasrightbutton btn btn-primary qflashcard-flipin mr-3', 'id' => 'qflashcard-iwasrightbutton-' . $qa->get_database_id()]);
-    $flipper .= html_writer::tag('button', $this->pix_icon('e/cancel', '') . get_string('iwaswrongbutton', 'qtype_flashcard'), ['class' => 'qflashcard-iwaswrongbutton btn btn-primary qflashcard-flipin', 'id' => 'qflashcard-iwaswrongbutton-' . $qa->get_database_id()]);
-    $flipper .= html_writer::end_tag('div');
-
-    $result = html_writer::tag('div', $flipper,
-      array('id' => 'qflashcard-flipcontainer-' . $qa->get_database_id(), 'class' => 'qflashcard-flipcontainer'));
-    if ($qa->get_state() == question_state::$invalid) {
-      $result .= html_writer::nonempty_tag('div',
-        $question->get_validation_error($qa->get_last_qt_data()),
-        array('class' => 'validationerror'));
+    protected function number_html($qnum) {
+        return $qnum . '. ';
     }
-    $context = ['qaid' => $qa->get_database_id()];
-    $PAGE->requires->js_call_amd('qtype_flashcard/flipquestion', 'init', $context);
-    return $result;
-  }
-
-  protected function number_html($qnum) {
-    return $qnum . '. ';
-  }
 
 }
