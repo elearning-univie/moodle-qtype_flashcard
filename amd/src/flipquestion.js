117d8dad8ad2438a8cc200488fdaf1dfbd4bc4e6
Adrian Czermak <adrian.czermak@univie.ac.at>
2020-01-09 13:16:18 +0100

    added input field for answer

--- amd/src/flipquestion.js
+++ amd/src/flipquestion.js
@@ -5,6 +5,9 @@ define(['jquery'], function($) {
                 var element = document.getElementById('qflashcard-flipcontainer-'.concat(qaid));
                 element.classList.toggle("flipped");
             };
+            $.qtype_flashcard_set_answer = function (qaid, qanswervalue) {
+                document.getElementById('qflashcard-question-answer-'.concat(qaid)).value = qanswervalue;
+            };
         }
     };
 });
